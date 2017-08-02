f29cf6201d6787449ea05628f89607d62b02b81e
Matt <mattchewone@gmail.com>
2017-08-02 18:05:51 +0100

    Increase timeouts to prevent the Axios AJAX request from timing out on a long upload

--- example/app.js
+++ example/app.js
@@ -1,11 +1,12 @@
-const feathers = require('feathers');
-const rest = require('feathers-rest');
-const socketio = require('feathers-socketio');
-const handler = require('feathers-errors/handler');
-const bodyParser = require('body-parser');
-const multer = require('multer');
-const multipartMiddleware = multer();
+const feathers = require('feathers')
+const rest = require('feathers-rest')
+const socketio = require('feathers-socketio')
+const handler = require('feathers-errors/handler')
+const bodyParser = require('body-parser')
+const multer = require('multer')
+const multipartMiddleware = multer()
 const path = require('path')
+const timeout = require('connect-timeout')
 
 const B2 = require('backblaze-b2')
 const { files: fileService, buckets: bucketService } = require('../lib/')
@@ -14,11 +15,15 @@ const { files: fileService, buckets: bucketService } = require('../lib/')
 const b2 = new B2({
   accountId: process.env['accountId'],
   applicationKey: process.env['applicationKey']
-});
+})
 // fileService options
 const options = {
   Model: b2
-};
+}
+
+function haltOnTimedout (req, res, next) {
+  if (!req.timedout) next()
+}
 
 // Create a feathers instance.
 const app = feathers()
@@ -36,27 +41,29 @@ const app = feathers()
   // add the file endpoint
   .use('/files',
     multipartMiddleware.single('file'),
+    // Add timeout to allow for length request
+    timeout('25m'),
 
     // another middleware, this time to
     // transfer the received file to feathers
     function(req,res,next){
-        req.feathers.file = req.file;
-        req.body.bucketId = 'insert-bucket-id-here'
-        next();
+        req.feathers.file = req.file
+        req.body.bucketId = 'cab869322dd730185dda001e'
+        next()
     },
-
+    haltOnTimedout,
     // use the fileService
     fileService(options)
   )
   .use('/buckets', bucketService(options))
 
 // A basic error handler, just like Express
-// app.use(handler());
+// app.use(handler())
 
 // Start the server if we're not being required in a test
 if (!module.parent) {
-  app.listen(3030);
-  console.log('Feathers B2 service running on http://127.0.0.1:3030');
+  app.listen(3030)
+  console.log('Feathers B2 service running on http://127.0.0.1:3030')
 }
 
-module.exports = app;
+module.exports = app
